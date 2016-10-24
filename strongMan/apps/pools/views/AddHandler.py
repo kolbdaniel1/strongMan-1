5a74ad089552061dc53c0e2d3ee2bd55c92cd657
Daniel Kolb <kolbdaniel1@gmail.com>
2016-10-24 13:03:48 +0100

    add 'add pool'-view and handlers

---
+++ strongMan/apps/pools/views/AddHandler.py
@@ -0,0 +1,28 @@
+from django.contrib import messages
+
+from ..forms import AddForm
+
+
+class AddHandler:
+    def __init__(self, is_add_form=False):
+        self.form = None
+        self.request = None
+        self.is_add_form = is_add_form
+
+    @classmethod
+    def by_request(cls, request, is_add_form=False):
+        handler = cls(is_add_form)
+        handler.request = request
+        return handler
+
+    def _render(self, form=AddForm()):
+            return self.request, 'pools/overview.html', {"form": form}
+
+    def handle(self):
+        self.form = AddForm(self.request.POST)
+        if not self.form.is_valid():
+            messages.add_message(self.request, messages.ERROR,
+                                 'Form was not valid')
+
+        return self._render(form=self.form)
+
