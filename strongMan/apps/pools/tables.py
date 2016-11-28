767d79500282c215640695d0562a15e65657da52
Daniel Kolb <kolbdaniel1@gmail.com>
2016-11-28 12:15:40 +0000

    fix detail pool view

--- strongMan/apps/pools/tables.py
+++ strongMan/apps/pools/tables.py
@@ -15,7 +15,7 @@ class PoolsTable(tables.Table):
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop("request")
-        # self.pooldetail =  kwargs.pop("pooldetail")
+        self.pools = kwargs.pop("pools")
         super(PoolsTable, self).__init__(*args, **kwargs)
 
     def render_removebtn(self, record):
@@ -27,38 +27,8 @@ class PoolsTable(tables.Table):
                                 request=self.request)
 
     def render_detail_collapse_column(self, record):
-        try:
-            pools = ViciWrapper().get_pools()
-            pool_details = {k: v for k, v in pools.items() if str(k) == str(record)}
-            size = 0
-            base = None
-            online = 0
-            offline = 0
-            leases = None
-
-            for key, value in pool_details[str(record)].items():
-                if key == 'size':
-                    size = value
-                elif key == 'base':
-                    base = value
-                elif key == 'online':
-                    online = value
-                elif key == 'offline':
-                    offline = value
-                elif key == 'leases':
-                    leases = value
-            leases = {0: {'address': '1.2.1.2',
-                          'identity': 'kdhjflkdhd@mail.com',
-                          'status': 'online'}}
-            return render_to_string('pools/widgets/detail_collapse_column.html', {'record': record,
-                                                                                  'detail': pool_details,
-                                                                                  'size': size, 'base': base,
-                                                                                  'online': online, 'offline': offline,
-                                                                                  'leases': leases},
+            return render_to_string('pools/widgets/detail_collapse_column.html', {'record': record},
                                     request=self.request)
-        except KeyError as k:
-            messages.add_message(self.request, messages.ERROR, k)
-            return redirect(reverse("pools:index"))
 
     class Meta:
         attrs = {"class": "table"}
